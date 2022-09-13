8fd2e84e49a1710d61f51035a3f65a44065292c1
Suleyman <suleyman20071998@gmail.com>
2022-09-13 14:09:59 +0300

    ex 5 has done

---
+++ es-5/exercise.js
@@ -0,0 +1,11 @@
+// Create the variables here
+
+// Don't modify this block. This is an if statement, you'll study it later in this course.
+let isDoorClosed = true;
+let isDogOutside = false;
+
+if (isDoorClosed == true && isDogOutside == false) {
+  console.log("the dor is closed and the dog is inside");
+} else if (isDoorClosed == false && isDogOutside == true) {
+  console.log("The door is open and the dog is outside");
+} else console.log("Modify the variables values");
