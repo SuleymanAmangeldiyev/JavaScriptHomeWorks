ef365a835ce38d6873b95c6f6ead7943cff00953
Suleyman <suleyman20071998@gmail.com>
2022-09-14 01:26:11 +0300

    the 17 ex has done

---
+++ es-17/exercise.js
@@ -0,0 +1,18 @@
+let age = 24,
+  age1 = 14;
+
+// console.log(age >= 18 ? "You can drive a car" : "You are too young to drive");
+// console.log(age1 >= 18 ? "You can drive a car" : "You are too young to drive");
+
+
+if(age >= 18){
+  console.log("You can drive a car");
+}else{
+  console.log("You are too young to drive");
+}
+//just to show it up how it works
+if (age1 >= 18) {
+  console.log("You can drive a car");
+} else {
+  console.log("You are too young to drive");
+}
