311cbc5b61a5fe94b67189ad0df428cec328a639
Suleyman <suleyman20071998@gmail.com>
2022-09-13 15:48:44 +0300

    exercise 8 has done

---
+++ es-8/exercise.js
@@ -0,0 +1,12 @@
+let num = true;
+console.log(num,'\t', typeof(num));
+
+num = Number(num);
+console.log(num, "\t", typeof num);
+
+num = String(num);
+console.log(num, "\t", typeof num);
+
+num = Boolean(num);
+console.log(num, "\t", typeof num);
+
