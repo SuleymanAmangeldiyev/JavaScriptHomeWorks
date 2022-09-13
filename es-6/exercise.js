2416c9e51ba140bf8d523f7f3e37fca48d77f9e8
Suleyman <suleyman20071998@gmail.com>
2022-09-13 14:18:35 +0300

    ex 6 has done

---
+++ es-6/exercise.js
@@ -0,0 +1,11 @@
+let hello = 'Ciao';     // boolean
+let age = 18;       // string
+let isGraduated = false;     // number
+
+Boolean(hello);
+String(age);
+Number(isGraduated);
+
+console.log(typeof(Boolean(hello)));
+console.log(typeof(String(age)));
+console.log(typeof(Number(isGraduated)));
