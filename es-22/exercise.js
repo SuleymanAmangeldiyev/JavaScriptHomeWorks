1fbee5510a065676b0c034a544dfe223842d4d43
Suleyman <suleyman20071998@gmail.com>
2022-09-14 02:44:13 +0300

    the ex 22 has done

---
+++ es-22/exercise.js
@@ -0,0 +1,38 @@
+// let primitive = 23;
+
+// primitive = typeof primitive;
+
+// if (primitive == "boolean") {
+//   console.log("It is a boolean!");
+// } else if (primitive == "string") {
+//   console.log("It is a string");
+// } else if (primitive == "number") {
+//   console.log("It is a number");
+// } else {
+//   console.log("It is undefind or there are some errors;");
+// }
+
+// primitive = "Suleyman";
+// console.log(typeof primitive);
+
+// primitive = true;
+// console.log(typeof primitive);
+
+let primitive;
+
+primitive = typeof(primitive);
+
+switch (primitive) {
+  case "boolean":
+    console.log("It is a boolean!");
+    break;
+  case "string":
+    console.log("It is a string");
+    break;
+  case "number":
+    console.log("It is a number");
+    break;
+  default:
+    console.log("It is undefind or there are some errors;");
+    break;
+}
