6b6f15f27209138a3595ddfaeaee99fc6166ae1d
Suleyman <suleyman20071998@gmail.com>
2022-09-14 00:59:16 +0300

    the ex 5 has done

---
+++ es-15/exercise.js
@@ -0,0 +1,5 @@
+let age = 24, age1 = 14;
+
+console.log(age >= 18 ? "You can drive a car" : "You are too young to drive");
+console.log(age1 >= 18 ? "You can drive a car" : "You are too young to drive");
+
