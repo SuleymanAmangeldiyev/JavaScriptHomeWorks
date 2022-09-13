8f646f7ef11d86ad2c5f7fa0dfdfa2fbf789cd6f
Suleyman <suleyman20071998@gmail.com>
2022-09-13 14:27:20 +0300

    exercise 7 has done

---
+++ es-7/exercise.js
@@ -0,0 +1,8 @@
+let hello = 'Ciao';     // number
+let age = 18;       // boolean
+let isGraduated = false;     // string
+
+// (age) will show (NoN : which means Not a Number)on the console, because it isn't a number that's why it cannot be converted to the number... 
+console.log(Number(hello));
+console.log(Boolean(age));
+console.log(String(isGraduated));
