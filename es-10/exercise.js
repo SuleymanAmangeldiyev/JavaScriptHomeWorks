940cb9cb3723526e3d027fa3f76b3c0dd0dfd20d
Suleyman <suleyman20071998@gmail.com>
2022-09-14 00:33:14 +0300

    the ex 10 has done

---
+++ es-10/exercise.js
@@ -0,0 +1,10 @@
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+
+//double equal operators trys to convert to the number the given data types and after that they compare them, if their numbers are equal to each other then it returns to true or if not they return to false 
+
+yearsCompleted == firstYearCompleted
+//the triple equal operators don't convert the datas to the numbers. They just compare them as they are, so if one side is boolean true and second side is 1 they aren't same with each other becuase boolean isn't a number and number isn't a boolean so this is the main difference between they.
+yearsCompleted === firstYearCompleted
+
