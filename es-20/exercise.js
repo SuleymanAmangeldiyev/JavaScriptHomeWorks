5dd1f5a66a263f274cfe5074487f9a64bf429902
Suleyman <suleyman20071998@gmail.com>
2022-09-14 02:26:03 +0300

    it is good now

--- es-20/exercise.js
+++ es-20/exercise.js
@@ -1,5 +1,16 @@
-let primitive = false;
-console.log(typeof(primitive));
+let primitive = 23;
+
+primitive = (typeof(primitive));
+
+if(primitive == "boolean"){
+  console.log("It is a boolean!");
+}else if(primitive == "string"){
+  console.log("It is a string");
+}else if(primitive == "number"){
+  console.log("It is a number");
+}else{
+  console.log("It is undefind or there are some errors;");
+}
 
 primitive = "Suleyman";
 console.log(typeof(primitive));
