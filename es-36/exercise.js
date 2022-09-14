b415b4df297fda4b8c23cf2a7bfb89a15313ca29
Suleyman <suleyman20071998@gmail.com>
2022-09-14 05:13:27 +0300

    ex 36 has done

---
+++ es-36/exercise.js
@@ -0,0 +1,15 @@
+//here is the global scope, just we need to declare our variable here...
+let sport = " Football";
+
+
+function canPlay() {
+
+  if (true) {
+    let personName = "Cosimo";
+    //here is the same as Exercise 35
+    console.log(personName + sport);
+  }
+
+}
+
+canPlay();
