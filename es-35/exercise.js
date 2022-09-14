0cebee80b6f214141b795589738f610a84d8afde
Suleyman <suleyman20071998@gmail.com>
2022-09-14 05:07:52 +0300

    the ex 35 has done

---
+++ es-35/exercise.js
@@ -0,0 +1,23 @@
+// function canPlay() {
+//   let sport = " Football";
+
+//   if (true) {
+//     let personName = "Cosimo";
+//   }
+//   console.log(personName + sport);
+
+// }
+
+// canPlay();
+
+function canPlay() {
+  let sport = " Football";
+
+  if (true) {
+    let personName = "Cosimo";
+ // we just need to move this line of code into if ocdes block, becuase which is declared by (let personName = "Cosimo") doesn't effect outside of the block so this is why we get an error 
+    console.log(personName + sport);
+  }
+}
+
+canPlay();
