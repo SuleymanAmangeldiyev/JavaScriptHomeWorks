f66979f352d6b0072a9e8eb2b4de51cb75b51966
Suleyman <suleyman20071998@gmail.com>
2022-09-15 15:55:01 +0300

    there are small changes

--- es-42/exercise.js
+++ es-42/exercise.js
@@ -8,12 +8,14 @@ let user = {
 // for(let key in user){
 //   newUser[key] = user[key];
 // }
-let newUser = {...user, name : "Paolo", id: 1};
+// let newUser = {...user, name : "Paolo", id: 1};
+let newUser = { ...user, name: "Paolo" };
+// let trr = [ 2, 3, 12, 2]
+// let arr = [...trr, 1,[43, 23, 43], 2];
+// let newArr = [...[...arr], ...trr, 3];
 
 
-
-
-console.log(newUser);
-console.log(user);
+console.log(newArr);
+console.log(arr);
 
 
