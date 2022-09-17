a3eea9ac876705accc7f828de78e14170aab172a
Suleyman <suleyman20071998@gmail.com>
2022-09-17 19:50:34 +0300

    the ex 46 has done

---
+++ es-46/exercise.js
@@ -0,0 +1,12 @@
+const student = {
+  id: 1,
+  school: "Liceo",
+  year: 3,
+  personalData: {
+    name: "Suleyman",
+    surname: "Amangeldiyev",
+    age: 24,
+  },
+};
+
+console.log(student.personalData);
