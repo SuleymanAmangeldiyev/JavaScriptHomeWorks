f8e266db22a33f6285ef728df2ad6cb17c7952cc
Suleyman <suleyman20071998@gmail.com>
2022-09-17 20:11:12 +0300

    the ex 48 has done

---
+++ es-48/exercise.js
@@ -0,0 +1,23 @@
+
+const students = [
+  (student1 = {
+    id: "723ABS",
+    name: "Mekan",
+    surname: "Amanow",
+    age: 30,
+  }),
+  (student1 = {
+    id: "7SE23AB",
+    name: "Amonowic",
+    surname: "Seyitnazar",
+    age: 24,
+  }),
+  (student1 = {
+    id: "7A23BS",
+    name: "Cary",
+    surname: "Mammetgeldi",
+    age: 20,
+  }),
+];
+
+console.log(students[0]);
