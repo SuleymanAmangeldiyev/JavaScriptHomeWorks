e19f9d345e4657d99d54e0d1b8061d8cbce15c15
Suleyman <suleyman20071998@gmail.com>
2022-09-17 20:00:19 +0300

    the ex 47 has done

---
+++ es-47/exercise.js
@@ -0,0 +1,21 @@
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+length = numbers.length;
+
+
+//this is the first element of the array
+console.log(numbers[0]);
+
+//this is the last element of the array
+console.log(numbers[length - 1]);
+
+//this is the array length
+console.log(length);
+
+
+//this line will add an element at the position 20th 
+numbers[19] = 32;
+
+//and from 11th till 19th will empty positions and they will undefined 
+console.log(numbers);
+
