896e2c239ed61a2613b9e2e230dee8246d9910b5
Suleyman Amangeldiyew <108282467+SuleymanAmangeldiyev@users.noreply.github.com>
2022-12-13 01:46:29 +0200

    corrected

--- es-53/exercise-1.js
+++ es-53/exercise-1.js
@@ -1,13 +1,13 @@
-function adultFilter(persons) {
- let final = 0;
-  for(let i = 0; i < persons.length; i++){
-    if(persons[i].age > 18 && final <= persons[i].age){
-      final = persons[i].age;
-      var toGoBack = persons[i].name + " " + persons[i].age;
-    }
-  }
-  return toGoBack;
-}
+// function adultFilter(persons) {
+//  let final = 0;
+//   for(let i = 0; i < persons.length; i++){
+//     if(persons[i].age > 18 && final <= persons[i].age){
+//       final = persons[i].age;
+//       var toGoBack = persons[i].name + " " + persons[i].age;
+//     }
+//   }
+//   return toGoBack;
+// }
 
 const persons = [
   { name: "Paul", age: 16 },
@@ -21,7 +21,7 @@ const persons = [
   { name: "Sandra", age: 34 },
   { name: "Alice", age: 28 },
 ];
-
+const adultFilter = (persons) => persons.filter(({age}) => age >= 18);
 const adults = adultFilter(persons);
 console.log(persons);
 console.log(adults);
